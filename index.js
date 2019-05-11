2bf62e97a966023d2633432f41c47d9d1df647fd
William Sinn <w.h.sinn@gmail.com>
2019-05-11 00:44:39 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 const driver = 'bob';
+let driverStr = `is the driver variable name`;
+
+function sentence() {
+    console.log("this code was called");
+}
+function car() {
+  console.log(driver);
+}
+function carDriver() {
+
+  console.log(driver, driverStr);
+
+}
+car(driver);
+sentence();
+carDriver(driver, driverStr);
